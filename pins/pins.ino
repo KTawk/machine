a8537d602662596c078794be92de4533e9dd1c10
kairlytauk <kairly.tauk@outlook.com>
2025-12-31 00:28:43 -0500

    adding everything else

---
+++ pins/pins.ino
@@ -0,0 +1,45 @@
+// ================= PINS ARM =================
+#define BASE_PIN        27
+#define SHOULDER_PIN    14
+#define ELBOW_PIN       12
+#define WRIST_ROT_PIN   1 //ok
+#define WRIST_BEND_PIN  13
+//#define RELAY_PIN       32 // NEW 
+
+// ======================================================
+// PINS – SUCTION (pompe + valve)
+// ======================================================
+#define PIN_POMPE 32
+#define PIN_VALVE 3 //ok
+
+// ======================================================
+// PINS – MECANUM / OMNI DRIVE
+// ======================================================
+#define STBY 33
+
+#define UL1 5
+#define UL2 17
+#define UR1 19
+#define UR2 18
+#define DL1 22
+#define DL2 21
+#define DR1 15
+#define DR2 2
+
+// ======================================================
+// PINS – LINEAR ACTUATOR (L298N / pont en H)
+// ======================================================
+#define ACT_IN1 23
+#define ACT_IN2 26
+
+// ======================================================
+// PARAMS
+// ======================================================
+#define DEADZONE 40
+#define MAX_PWM  255
+
+
+
+
+// 25 and 16 for relay
+// 1 and 3 for rotation and valve
